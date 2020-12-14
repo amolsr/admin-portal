f4eb61df4fc52de3bbea262326a706109b905fa3
amolsr <amol.saini567@gmail.com>
2020-12-15 01:02:54 +0530

    theme added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import deepOrange from "@material-ui/core/colors/deepOrange";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#3e4f60" },
+    secondary: {
+      main: "#f7932e",
+    },
+  },
+});
+export default theme;
